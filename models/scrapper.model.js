97b4e7c4989f796026372c929c774ce75b4cefc2
Nur Muhamad Ash Shidiqi <nurmuhamad.a.13@gmail.com>
2020-11-23 15:06:38 +0700

    Remove getHtml method

--- models/scrapper.model.js
+++ models/scrapper.model.js
@@ -5,13 +5,10 @@ require('dotenv').config();
 const listUrl = `${process.env.BASE_URL}Daftar_tempat_wisata_di_Indonesia`;
 
 class scrapperModel {
-  static async getHtml () {
-    this._html = await rp(listUrl);
-  }
-
   static async getDestinationsList () {
     let listOfDestinations = [];
-    const destinations = await $('.mw-parser-output ul li>a', this._html).toArray();
+    const html = await rp(listUrl);
+    const destinations = await $('.mw-parser-output ul li>a', html).toArray();
     destinations.forEach((destination, index) => {
       const { title: name, href } = destination.attribs;
       if (href.includes('/wiki/')) {
