f1ff4e3f31acacd5f4ee75805aea7452a1304b20
Nur Muhamad Ash Shidiqi <nurmuhamad.a.13@gmail.com>
2020-11-25 07:50:32 +0700

    Add insert data method

--- models/export.model.js
+++ models/export.model.js
@@ -19,6 +19,19 @@ class exportModel {
     worksheet.getRow(1).alignment = style.center;
 
     await workbook.xlsx.writeFile(path);
+
+    return "File successfully generated";
+  }
+
+  static async insertRow ({ path, sheetName, data }) {
+    const workbook = new Excel.Workbook();
+    await workbook.xlsx.readFile(path)
+
+    const worksheet = workbook.getWorksheet(sheetName);
+    worksheet.addRow(data).commit();
+    await workbook.xlsx.writeFile(path);
+
+    return "Data successfully added";
   }
 }
 
