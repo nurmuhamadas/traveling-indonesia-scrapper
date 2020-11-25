fa8abf9aff71d7fbe0db5137236f45d8ba2c2c7e
Nur Muhamad Ash Shidiqi <nurmuhamad.a.13@gmail.com>
2020-11-25 10:01:43 +0700

    change  object structure of excel model

--- models/excel.model.js
+++ models/excel.model.js
@@ -2,28 +2,27 @@ const Excel = require('exceljs');
 const { columnTemplate, style } = require('../utils/excelProp');
 
 class exportModel {
-  static async generateFile ({ path, sheetName }) {
-    const workbook = new Excel.Workbook();
-    const worksheet = workbook.addWorksheet(sheetName);
+  constructor ({ sheetName, path}) {
+    this._path = path;
+    this._workbook = new Excel.Workbook();
+    this._worksheet = this._workbook.addWorksheet(sheetName);
+  }
 
-    worksheet.columns = await columnTemplate;
+  async generateFile () {
+    this._worksheet.columns = await columnTemplate;
 
-    worksheet.getRow(1).fill = style.fill;
-    worksheet.getRow(1).font = style.bold;
-    worksheet.getRow(1).alignment = style.center;
+    this._worksheet.getRow(1).fill = style.fill;
+    this._worksheet.getRow(1).font = style.bold;
+    this._worksheet.getRow(1).alignment = style.center;
 
-    await workbook.xlsx.writeFile(path);
+    await this._workbook.xlsx.writeFile(this._path);
 
     return "File successfully generated";
   }
 
-  static async insertRow ({ path, sheetName, data }) {
-    const workbook = new Excel.Workbook();
-    await workbook.xlsx.readFile(path)
-
-    const worksheet = workbook.getWorksheet(sheetName);
-    worksheet.addRow(data).commit();
-    await workbook.xlsx.writeFile(path);
+  async insertRow (data) {
+    this._worksheet.addRow(data).commit();
+    await this._workbook.xlsx.writeFile(this._path);
 
     return "Data successfully added";
   }
